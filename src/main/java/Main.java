75131e62d6698e3f6e967ac491baff31ad1b71e4
Chirag Jaiswal <chiragwork2403@gmail.com>
2025-06-13 19:42:26 +0530

    type command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,9 +11,26 @@ public class Main {
             switch (command) {
                 case "exit" -> System.exit(0);
                 case "echo" -> System.out.println(input.split(" ", 2)[1]);
+                case "type" -> type( input );
                 default -> System.err.printf("%s: command not found\n", input);
             }
             System.out.print("$ ");
         }
+    }
+    static void type ( String input ){
+        String validCommands[] = { "exit" , "echo" , "type" };
+        String command = input.split(" ",2)[1] ;
+        boolean isPresent = false ;
+        for ( String validCommand :  validCommands ){
+            if ( validCommand.equals(command) ){
+                isPresent = true ;
+                break ;
+            }
+        }
+        if ( isPresent ){
+            System.out.printf("%s is a shell builtin\n",command);
+        }
+        else
+            System.out.printf("%s: not found\n",command);
     }
 }
