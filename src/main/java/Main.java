d541deb99fb1a05a51050362f7c6db581c8b717c
Chirag Jaiswal <chiragwork2403@gmail.com>
2025-06-13 19:17:19 +0530

    exit built in refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,19 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-         System.out.print("$ ");
-
+        System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
         while ( true ) {
             String input = scanner.nextLine();
-            if ( input.equals("exit 0") ){
-                System.exit(0);
-            }
-            else {
-                System.out.println(input + ": command not found");
-                System.out.print("$ ");
-            }
-        }
+            if ( input.equals("exit 0") ) System.exit(0);
 
+            System.out.println(input + ": command not found");
+            System.out.print("$ ");
+
+        }
     }
 }
