bcdba13a6d8cb31d125fba0930fae2a976597334
Chirag Jaiswal <chiragwork2403@gmail.com>
2025-06-13 19:11:00 +0530

    repl test 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ public class Main {
         while ( true ) {
             String input = scanner.nextLine();
             System.out.println(input + ": command not found");
+            System.out.print("$ ");
         }
 
     }
